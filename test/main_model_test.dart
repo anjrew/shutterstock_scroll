a340589ce5342c587913bdd9ae9eda5dd0d698ba
earyzhe <earyzhe@gmail.com>
2019-07-08 11:47:02 +0200

    Starting to mock tests

---
+++ test/main_model_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+import 'package:shutterstock_scroll/logic/main_model.dart';
+
+void main() {
+  test('Counter value should be incremented', () {
+    final model = MainModel();
+
+    
+
+    expect(counter.value, 1);
+  });
+}
