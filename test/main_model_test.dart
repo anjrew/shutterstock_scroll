fa7970605582910df9546817cf448310ff642934
earyzhe <earyzhe@gmail.com>
2019-07-08 12:01:21 +0200

    Try to make tests

--- test/main_model_test.dart
+++ test/main_model_test.dart
@@ -1,12 +1,15 @@
-import 'package:test/test.dart';
 import 'package:shutterstock_scroll/logic/main_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+
+class MockClient extends Mock implements http.Client {}
 
 void main() {
-  test('Counter value should be incremented', () {
-    final model = MainModel();
+    test('Counter value should be incremented', () {
+        final model = MainModel();
+        expect(model.pageNumber, 1);
+    });
 
-    
-
-    expect(counter.value, 1);
-  });
+    test(description, body)
 }
