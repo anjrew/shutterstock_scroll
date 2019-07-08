bd18fec26b55cdda3e125b3eaa86314dfa9b2a61
earyzhe <earyzhe@gmail.com>
2019-07-08 14:50:12 +0200

    Tring to mack http request

--- test/main_model_test.dart
+++ test/main_model_test.dart
@@ -1,12 +1,16 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:shutterstock_scroll/logic/main_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class MockHttp extends Mock implements http.Client {}
 
 void main() {
-    final mockHttp = MockHttp();
+    http.Client mockHttp = MockHttp();
     final model = MainModel();
 
     test('Counter value should be incremented', () {
@@ -15,11 +19,20 @@ void main() {
 
     });
 
-    test('Get more images increments', (){
-        when(mockHttp.get('https://api.shutterstock.com/v2/images/search?per_page=10&&page=1'))
-                .thenAnswer((Invocation invocation) 
-                    async => http.Response('This is a JSON',200));
+    test('Get more images increments', () async {
 
-        expect(model.getimages(1), isFlutterError);
+        final mockDataFile = new File('test/mock_data/shutterstock_images_mock_data.json');
+        final jsonData = json.decode(await mockDataFile.readAsString());
+
+        when(mockHttp.get('https://api.shutterstock.com/v2/images/search?per_page=10&&page=1'))
+                .thenAnswer((_) => Future.value(Response(jsonData, 200)));
+
+        var mockedResult = await model.getimages(1);
+
+        expect(mockedResult, {
+            "url": "http://saohiazo.jp/mup",
+            "description": "Ace nibnofrik hin ita afcaz zez mub lew dudotep ekitoco tamnij oj jezesi kujut kawzikuf ukrigrej lunefub jeom. Mazojatu so ak fahzilic pozus emenu rudhuv olarih wewapga nukve hapus wocat la sup wospefah. Tu binas zecucko ah unpelmih vevbej vubuoro wi ka wikin betores momkelbi. Souwva jura oziforho pame kewzejtu ja huju lon mes zegag nocpobez huka. Uvtukfab zughozeh iniwudni fu uzewes hitdutzob sijo kosij afe vauru ozgibpos rurwac morja licbavze gurwatwev. We dofra rohotuz kag vet kefga hih hifesuh higejnet nokjob joupo zaw finusiw doge vi opiahuec.",
+            "id": 257290
+        });
     });
 }
