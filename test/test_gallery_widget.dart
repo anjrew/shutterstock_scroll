58536f02a2b1e448a6b8033ac2207592541b7c9b
earyzhe <earyzhe@gmail.com>
2019-07-18 14:22:18 +0100

    Gallery list golden test implemented

--- test/test_gallery_widget.dart
+++ test/test_gallery_widget.dart
@@ -1,17 +1,13 @@
 import 'dart:io';
 import 'package:http/http.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'package:shimmer/shimmer.dart';
 import 'package:http/testing.dart';
-import 'package:shutterstock_scroll/classes/image_data.dart';
 import 'package:shutterstock_scroll/logic/main_model.dart';
 import 'package:shutterstock_scroll/pages/main_page/widgets/gallery_list.dart';
 import 'package:shutterstock_scroll/pages/main_page/widgets/square_image.dart';
-import 'package:shutterstock_scroll/widgets/error_widget/shutterstock_loader.dart';
 
 void main() {
     Client mockHttp = MockClient((request) async {
@@ -56,14 +52,8 @@ void main() {
         final testWidget = GalleryListWidgetWrapper(mainModel: mainModel);
 
         await tester.pumpWidget(testWidget);
-        // expect(find.byType(SSLoader).first, findsOneWidget);
         expect(find.byKey(ValueKey('GalleryList')), findsOneWidget);
-
-        // expect(find.byWidget(testWidget), findsOneWidget);
-        // // expect(testWidget.imageData, imageData);
-        // expect(imageFinder, findsOneWidget);
-        // expect(shimmerFinder, findsOneWidget);
-        // expect(heroFinder, findsOneWidget);
+		expect(find.byType(SquareImage), findsWidgets);
     });
 
     testWidgets("Test UI layout", (WidgetTester tester) async {
@@ -73,7 +63,7 @@ void main() {
 
         await expectLater(
             find.byType(GalleryListWidgetWrapper),
-            matchesGoldenFile('golden/test_square_image_ui.png'),
+            matchesGoldenFile('golden/gallery_list_image_ui.png'),
             skip: !Platform.isMacOS,
         );
     });
